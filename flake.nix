95e85d0c095f8c3a0ecaa535bddcbcc1def35a6b
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2023-08-16 17:42:25 -0300

    Setup nix flake and shell

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "graphs";
+
+        packages = with pkgs; [
+          gnumake
+          clang_16
+          clang-tools_16
+        ];
+      };
+    });
+}
